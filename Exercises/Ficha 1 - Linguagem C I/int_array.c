31508fa6bd6f4c1cd7c8ecb68cc04825059753ef
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-03-11 23:54:37 +0000

    int

--- Exercises/Ficha 1 - Linguagem C I/int_array.c
+++ Exercises/Ficha 1 - Linguagem C I/int_array.c
@@ -2,7 +2,7 @@
 
 int main() {
     int i;
-    int primes[] = {2, 3, 5, 7, 11};
+    int primes[] = {2, 3, 5, 7, 11}; // cada int tem 4 bytes
     for (i = 0; i < sizeof(primes)/sizeof(int); i++) {
         printf("%d <--> %d\n", primes[i], *(primes + i));
     }

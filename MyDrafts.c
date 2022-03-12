fc468f7e561e8adbd38e69068e2e72daf6edeae7
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-03-12 00:39:02 +0000

    drafts update

--- MyDrafts.c
+++ MyDrafts.c
@@ -90,3 +90,15 @@ int main() {
     return 0;
 }
 
+int* get_int() {
+    int* p = (int*)malloc(sizeof(int));
+    *p =2;
+    return p;
+}
+
+int main() {
+    int* p = get_int();
+    printf("integer = %d\n", *p);
+    return 0;
+}
+

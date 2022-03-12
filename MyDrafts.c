86b6c01f2ac903be2bc22aca8bdf843bec7343a2
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-03-12 03:16:52 +0000

    drafts update

--- MyDrafts.c
+++ MyDrafts.c
@@ -221,3 +221,53 @@ int main() {
     printf("tan(60) = %f\n", sin_degrees(60) / cos_degrees(60));
     return 0;
 }
+
+/*
+ * implementation of the Complex API
+*/
+
+complex* complex_new(double x, double y) {
+    complex* z = (complex*) malloc(sizeof(complex));
+    z->x = x;
+    z->y = y;
+    return z;
+}
+
+complex* complex_add(complex* z, complex* w){
+    return complex_new(z->x + w->x, z->y + w->y);
+}
+
+complex* complex_sub(complex* z, complex* w){
+    return complex_new(z->x - w->x, z->y - w->y);
+}
+
+complex* complex_mul(complex* z, complex* w){
+    return complex_new(z->x * w->x - z->y * w->y,
+                       z->x * w->y + z->y * w->x);
+}
+
+complex* complex_div(complex* z, complex* w){
+    return complex_new(z->x / w->x - z->y / w->y,
+                       z->x / w->y + z->y / w->x);
+}
+
+complex* complex_conj(complex* z){
+    return complex_new(z->x , z->y);
+}
+
+double   complex_mod(complex* z){
+    return fabs(z->x);
+}
+
+double   complex_arg(complex* z){
+    return atan2(z->y,z->x);
+}
+
+double   complex_re(complex* z){
+    return z->x;
+}
+
+double   complex_im(complex* z){
+    return z->y;
+}
+

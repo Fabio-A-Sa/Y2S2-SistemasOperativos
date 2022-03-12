9b0229777684659004463ad1c44490ad429f2120
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-03-12 03:20:39 +0000

    drafts update

--- MyDrafts.c
+++ MyDrafts.c
@@ -696,3 +696,34 @@ int main(int argc, char** argv) {
     return 0;
 }
 
+vector* vector_new(double x, double y, double z) {
+    vector* result = (vector*) malloc (sizeof(vector));
+    result->x = x;
+    result->y = y;
+    result->z = z;
+    return result;
+}
+
+vector* vector_add(vector* a, vector* b) {
+    return vector_new(a->x + b->x, a->y + b->y, a->z + b->z);
+}
+
+vector* vector_sub(vector* a, vector* b) {
+    return vector_new(a->x - b->x, a->y - b->y, a->z - b->z);
+}
+
+vector* vector_scale(double a, vector* b) {
+    return vector_new(a * b->x, a * b->y, a * b->z);
+}
+
+vector* vector_vprod(vector* a, vector* b) {
+    return vector_new(a->x * b->x, a->y * b->y, a->z * b->z);
+}
+
+double  vector_sprod(vector* a, vector* b) {
+    return a->x * b->x + a->y * b->y + a->z * b->z;
+}
+
+double  vector_mod(vector* a) {
+    return sqrt(pow(a->x, 2) + pow(a->y, 2) + pow(a->z, 2));
+}

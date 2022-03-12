915a24b55ccab4a9cb1a10b4e04c014f8d78d688
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-03-12 00:39:30 +0000

    drafts update

--- MyDrafts.c
+++ MyDrafts.c
@@ -107,3 +107,12 @@ int main() {
     return 0;
 }
 
+int main() {
+    int i;
+    int primes[] = {2, 3, 5, 7, 11}; // cada int tem 4 bytes
+    for (i = 0; i < sizeof(primes)/sizeof(int); i++) {
+        printf("%d <--> %d\n", primes[i], *(primes + i));
+    }
+    return 0;
+}
+

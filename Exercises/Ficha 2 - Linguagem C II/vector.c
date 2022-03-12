333580bc3ab7de1194f001640daf49c071cfdb26
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-03-12 01:21:35 +0000

    implementation

---
+++ Exercises/Ficha 2 - Linguagem C II/vector.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <math.h>
+#include "vector.h"
+
+vector* vector_new(double x, double y, double z) {
+    vector* result = (vector*) malloc (sizeof(vector));
+    result->x = x;
+    result->y = y;
+    result->z = z;
+    return result;
+}
+
+vector* vector_add(vector* a, vector* b) {
+    return vector_new(a->x + b->x, a->y + b->y, a->z + b->z);
+}
+
+vector* vector_sub(vector*, vector*) {
+    return vector_new(a->x - b->x, a->y - b->y, a->z - b->z);
+}
+
+vector* vector_scale(double a, vector* b) {
+    return vector_new(a * b->x, a * b->y, a * b->z);
+}
+
+vector* vector_vprod(vector* a, vector* b) {
+    return vector_new(a->x * b->x, a->y * b->y, a->z * b->z);
+}
+
+double  vector_sprod(vector* a, vector* b) {
+    return a->x * b->x + a->y * b->y + a->z * b->z;
+}
+
+double  vector_mod(vector* a) {
+    return sqrt(pow(a->x, 2) + pow(a->y, 2) + pow(a->z, 2));
+}

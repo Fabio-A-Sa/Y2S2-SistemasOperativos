9915eb20fa4d073871491aac9964d536cb27f5be
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-03-12 03:19:56 +0000

    drafts update

--- MyDrafts.c
+++ MyDrafts.c
@@ -639,3 +639,18 @@ int main(int argc, char** argv) {
     return 0;
 }
 
+int main(int argc, char** argv) {
+
+    list* l = list_new_random(20, 100);
+    list_print(l);
+
+    printf("First: %d\n", list_get_first(l));
+    printf("Last: %d\n", list_get_last(l));
+
+    printf("Remove first: ");
+    list_remove_first(l); list_print(l);
+    printf("Remove last: ");
+    list_remove_last(l); list_print(l);
+
+    return 0;
+}

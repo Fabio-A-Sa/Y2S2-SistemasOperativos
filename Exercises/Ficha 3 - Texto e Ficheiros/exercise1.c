d9e19e971568396479adaec41c3b53dd4ff7cca9
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-03-22 14:49:35 +0000

    J

--- Exercises/Ficha 3 - Texto e Ficheiros/exercise1.c
+++ Exercises/Ficha 3 - Texto e Ficheiros/exercise1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #define MAX_STR_SIZE  64
 
@@ -9,7 +10,13 @@ int main(int argc, char* argv[]) {
     char* string = (char*)malloc(MAX_STR_SIZE * sizeof(char));
     strcpy(string, argv[1]);
 
-    printf("String %s\n", string);
+    printf("Initial string: %s\n", string);
+
+    for (char i = 0 ; i < strlen(string) ; i++) {
+        *(string + i) = tolower( (unsigned char) *(string+i));
+    }
+
+    printf("Lower letters: %s\n", string);
 
     return 0;
 }

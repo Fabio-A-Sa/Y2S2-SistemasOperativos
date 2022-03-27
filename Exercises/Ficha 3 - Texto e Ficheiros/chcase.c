765c90be638fd84a1ec1db84fc6778722d026365
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-03-27 17:34:04 +0100

    added content

---
+++ Exercises/Ficha 3 - Texto e Ficheiros/chcase.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+void upperCase(char* fileName) {
+
+    FILE* file = fopen(fileName, "r");
+    const int MAX_SIZE = 100;
+    char content[MAX_SIZE];
+    fseek(file, 0, SEEK_SET);
+    fread(content, sizeof(char) * MAX_SIZE, MAX_SIZE, file);
+
+    int i = 0;
+    while (*(content + i) != '\0' && *(content + i) != NULL) {
+        printf("%c", toupper( (unsigned char) *(content+i)));
+        i++;
+    }
+    printf("\n");
+    fclose(file);
+}
+
+void lowerCase(char* fileName) {
+
+    FILE* file = fopen(fileName, "r");
+    const int MAX_SIZE = 100;
+    char content[MAX_SIZE];
+    fseek(file, 0, SEEK_SET);
+    fread(content, sizeof(char) * MAX_SIZE, MAX_SIZE, file);
+
+    int i = 0;
+    while (*(content + i) != '\0' && *(content + i) != NULL) {
+        printf("%c", tolower( (unsigned char) *(content+i)));
+        i++;
+    }
+    printf("\n");
+    fclose(file);
+}
+
+int main (int arg, char* argv[]) {
+
+    int comparation;
+    char* fileName = argv[1];
+    char* mode = argv[2];
+
+    comparation = strcmp(mode, "-u");
+    if (comparation == 0) upperCase(fileName);
+    comparation = strcmp(mode, "-l");
+    if (comparation == 0) lowerCase(fileName);
+    else printf("Input error!\n");
+
+    return 0;
+}

2a582c159854ce1410c9d0da58b3a663fa19deef
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-03-27 17:47:10 +0100

    version

--- Exercises/Ficha 3 - Texto e Ficheiros/cp.c
+++ Exercises/Ficha 3 - Texto e Ficheiros/cp.c
@@ -3,9 +3,30 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+void createFile (char* fileName) {
+
+
+}
+
+void copyFiles (char* file1, char* file2) {
+
+
+}
+
 int main (int arg, char* argv[]) {
 
-    
+    char* file1 = argv[1];
+    char* file2 = argv[2];
+
+    if (file1 == NULL) {
+        printf(" $ ./a.out <file1> [file2]\n");
+    } else if (file2 == NULL) {
+        char* newName = "file2";
+        createFile(newName);
+        file2 = newName;
+    }
+
+    copyFiles(file1, file2);
 
     return 0;
 }

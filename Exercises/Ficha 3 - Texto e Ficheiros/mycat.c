6c362c31a3ff4788a09a3c21ef3c1a597293b91c
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-03-27 16:35:24 +0100

    double and single files

--- Exercises/Ficha 3 - Texto e Ficheiros/mycat.c
+++ Exercises/Ficha 3 - Texto e Ficheiros/mycat.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main () {
+void singleFile(char* argv[]) {
 
-    printf("Hello World\n");
+    char* file = argv[1];
+    printf("Single file called %s!\n", file);
+}
+
+void twoFiles(char* argv[]) {
+
+    char* file1 = argv[1];
+    char* file2 = argv[2];
+    printf("Double file called %s and %s!\n", file1, file2);
+}
+
+
+int main (int argc, char* argv[]) {
+
+    if (argv[2] != NULL) twoFiles(argv); else singleFile(argv);
     return 0;
 }

45a9f870396a435efc59dfc945aaa67be4144e9c
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-03-27 23:11:01 +0100

    myCat

--- Exercises/Ficha 3 - Texto e Ficheiros/mycat.c
+++ Exercises/Ficha 3 - Texto e Ficheiros/mycat.c
@@ -7,14 +7,14 @@ void showFile(char* fileName) {
     const int MAX_SIZE = 100;
     char content[MAX_SIZE];
     fseek(file, 0, SEEK_SET);
-    int finish = fread(content, sizeof(char) * MAX_SIZE, MAX_SIZE, file);
+    int finish = fread(content, sizeof(char), MAX_SIZE, file);
     content[finish] = '\0';
 
     int i = 0;
     while (*(content + i) != '\0') {
         printf("%c", *(content + i));
         i++;
-    } printf("\n");
+    }
     
     fclose(file);
 }

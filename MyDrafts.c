95ff82577d8fc2742cfb65b9fa7fc27b00d7e701
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-03-28 01:17:12 +0100

    drafts update

--- MyDrafts.c
+++ MyDrafts.c
@@ -1016,3 +1016,44 @@ int main (int args, char* argv[]) {
     return 0;
 }
 
+int main (int arg, char* argv[]) {
+
+    int answer = 0, index = 0;
+    char* fileName = argv[1];
+    char* mode = argv[2];
+
+    if (mode == NULL) {
+        printf("Input error!\n");
+        return -1;
+    }
+
+    char content[MAX_SIZE];
+
+    FILE* file = fopen(fileName, "r");
+    int finish = fread(content, 1, sizeof(content), file);
+    fclose(file);
+
+    if (strcmp(mode, "-l") == 0) {
+
+        while (index != finish) {
+            if (content[index] == '\n') answer++;
+            index++;
+        }
+
+    } else if (strcmp(mode, "-w") == 0) {
+        
+        while (index != finish) {
+            if (content[index] == ' ' || content[index] == '\n') answer++;
+            index++;
+        } answer++;
+
+    } else {
+        answer = finish;
+    }
+
+    printf("Content:\n%s\n", content);
+    printf("Result: %d\n", answer);
+
+    return 0;
+}
+

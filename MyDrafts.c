421f8e7160b9682ef52c078491b05777fcdfe843
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-03-28 01:15:50 +0100

    drafts update

--- MyDrafts.c
+++ MyDrafts.c
@@ -894,3 +894,32 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
+void showFile(char* fileName) {
+
+    FILE* file = fopen(fileName, "r");
+    const int MAX_SIZE = 100;
+    char content[MAX_SIZE];
+    fseek(file, 0, SEEK_SET);
+    int finish = fread(content, sizeof(char), MAX_SIZE, file);
+    content[finish] = '\0';
+
+    int i = 0;
+    while (*(content + i) != '\0') {
+        printf("%c", *(content + i));
+        i++;
+    }
+    
+    fclose(file);
+}
+
+int main (int argc, char* argv[]) {
+
+    int i = 1;
+    while (argv[i] != NULL) {
+        showFile(argv[i]);
+        i++;
+    }
+
+    return 0;
+}
+

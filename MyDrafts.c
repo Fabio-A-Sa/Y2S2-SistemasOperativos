2bac5bea0b5276da023fb6c8f6aa57c07aa80bfb
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-03-28 01:16:25 +0100

    drafts update

--- MyDrafts.c
+++ MyDrafts.c
@@ -960,3 +960,59 @@ int main (int arg, char* argv[]) {
     return 0;
 }
 
+#define MAX_SIZE 1024
+
+int main (int args, char* argv[]) {
+
+    char* word = argv[1];
+    char* fileName = argv[2];
+
+    if (word == NULL || fileName == NULL) {
+        printf("Invalid input!\n");
+        return -1;
+    }
+
+    char content[MAX_SIZE];
+    FILE* file = fopen(fileName, "r");
+    int finish = fread(content, sizeof(char), MAX_SIZE, file);
+    fclose(file);
+    content[finish] = '\0';
+
+    /* Output
+    printf("Finish: %d\n", finish);
+    printf("Content: %s\n", content);
+    */
+
+    char currentLine[MAX_SIZE];
+    int i = 0, line = 0;
+
+    for (int index = 0 ; index <= finish ; index++) {
+
+        if (content[index] == '\n') {
+
+            currentLine[i] = '\0';
+            i = 0;
+            char* result = strstr(currentLine, word);
+
+            /* Output
+            printf("Linha %d: %s\n", line, currentLine);
+            if (result != NULL) {
+                printf("%s ocorre na linha %d\n\n", word, line);
+            } else printf("%s não ocorre na linha %d\n\n", word, line);
+            */
+
+            if (result != NULL) {
+                printf("[%d:%d]\n", line, index);
+            }
+
+            line++;
+
+        } else {
+            currentLine[i] = content[index];
+            i++;
+        }
+    }
+
+    return 0;
+}
+

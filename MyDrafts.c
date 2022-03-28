49ddaed56c2c029021c336775131ef734ee8b80c
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-03-28 01:14:56 +0100

    drafts update

--- MyDrafts.c
+++ MyDrafts.c
@@ -743,3 +743,63 @@ vector* vector_scale(double, vector*);
 vector* vector_vprod(vector*, vector*);
 double  vector_sprod(vector*, vector*);
 double  vector_mod(vector*);
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int upperCase(char* fileName) {
+
+    FILE* file = fopen(fileName, "r");
+    const int MAX_SIZE = 100;
+    char content[MAX_SIZE];
+    fseek(file, 0, SEEK_SET);
+    int finish = fread(content, sizeof(char), MAX_SIZE, file);
+    content[finish] = '\0';
+
+    int i = 0;
+    while (*(content + i) != '\0') {
+        printf("%c", toupper( (unsigned char) *(content+i)));
+        i++;
+    }
+    printf("\n");
+    fclose(file);
+
+    return 0;
+}
+
+int lowerCase(char* fileName) {
+
+    FILE* file = fopen(fileName, "r");
+    const int MAX_SIZE = 100;
+    char content[MAX_SIZE];
+    fseek(file, 0, SEEK_SET);
+    int finish = fread(content, sizeof(char), MAX_SIZE, file);
+    content[finish] = '\0';
+
+    int i = 0;
+    while (*(content + i) != '\0') {
+        printf("%c", tolower( (unsigned char) *(content+i)));
+        i++;
+    }
+    printf("\n");
+    fclose(file);
+
+    return 0;
+}
+
+int main (int arg, char* argv[]) {
+
+    int comparation;
+    char* fileName = argv[1];
+    char* mode = argv[2];
+
+    comparation = strcmp(mode, "-u");
+    if (comparation == 0) return upperCase(fileName);
+    comparation = strcmp(mode, "-l");
+    if (comparation == 0) return lowerCase(fileName);
+    else printf("Input error!\n");
+
+    return 0;
+}

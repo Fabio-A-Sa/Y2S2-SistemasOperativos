0962771999a5a5a65fcd79817673e4585ee7e548
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-04-11 20:39:05 +0100

    new script

--- Exercises/Ficha 4 - API do Kernel/testMyChmode.sh
+++ Exercises/Ficha 4 - API do Kernel/testMyChmode.sh
@@ -1,6 +1,8 @@
 rm -r testfile.txt
-echo "first line\nsecond line" >> testfile.txt
+echo "first line" >> testfile.txt
+echo "second line" >> testfile.txt
 gcc mychmode.c
 ls -l testfile.txt
 ./a.out 755 testfile.txt
+ls -l testfile.txt
 ./a.out 799 testfile.txt

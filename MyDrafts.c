f79e907af6e0220c27cb72c29ca8fd8f376b3f73
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-04-12 19:03:56 +0100

    drafts update

--- MyDrafts.c
+++ MyDrafts.c
@@ -1363,3 +1363,33 @@ int main(int argc, char* argv[]) {
     return EXIT_SUCCESS;
 }
 
+int main(int argc, char* argv[]) {
+
+    pid_t pid;
+
+    /* fork a child process */
+    if ((pid = fork()) == -1) {
+        perror("fork");
+        return EXIT_FAILURE;
+
+    } else if (pid == 0) {
+        /* child process */
+        if (execlp(argv[1], argv[1], NULL) == -1) {
+            perror("execlp");
+            return EXIT_FAILURE;
+        }
+
+    } else {
+        /* parent process */
+        if (waitpid(pid, NULL, 0) == -1) {
+            perror("waitpid");
+            return EXIT_FAILURE;
+        }
+        printf("child exited\n");
+    }
+
+    /*  A system call 'execlp' faz com que o processo filho seja chamado para o código especificado em argv[1]
+        Quando termina, retorna e o processo pai é avisado através da system call wait()                           */
+    return EXIT_SUCCESS;
+}
+

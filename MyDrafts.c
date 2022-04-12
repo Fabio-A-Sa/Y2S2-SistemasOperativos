5e4948dc4dee4c04d10cc90461825c69868cd58d
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-04-12 15:22:42 +0100

    drafts update

--- MyDrafts.c
+++ MyDrafts.c
@@ -1242,3 +1242,32 @@ int main (int args, char* argv[]) {
     return EXIT_SUCCESS;
 }
 
+void getFileStatus(char *fileName) {
+
+    struct stat info;
+
+    if (lstat(fileName, &info) == -1) {
+        fprintf(stderr, "fsize: Can’t stat %s\n", fileName);
+        return;
+    }
+
+    printf("%s\nsize: %d bytes\ndisk_blocks: %d\nlast edit: %sowner: %d\n",
+            fileName, (int)info.st_size, (int)info.st_blocks, ctime(&info.st_mtime), info.st_gid);
+}
+
+int main(int argc, char* argv[]) {
+
+    if (argc < 2) {
+        fprintf(stderr, "usage: %s file\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    int i = 1;
+    while (argv[i] != NULL) {
+        getFileStatus(argv[i]);
+        i++;
+    }
+
+    return EXIT_SUCCESS;
+}
+

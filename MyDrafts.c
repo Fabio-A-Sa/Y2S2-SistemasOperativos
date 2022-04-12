09a38bbfddecb4953a72e26ef1ea9c233233b409
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-04-12 19:03:31 +0100

    drafts update

--- MyDrafts.c
+++ MyDrafts.c
@@ -1319,3 +1319,17 @@ int main(int argc, char* argv[]) {
     return EXIT_SUCCESS;
 }
 
+int main(int argc, char* argv[]) {
+
+    int v;
+    printf("Initial process: %d\n", getpid());
+
+    for (int i = 0; i < 4; i++) {
+        v = fork();
+        if (v == 0) printf("New process %d: %d\n", i, getpid());
+    }
+
+    // Total = 16 processes (2^4)
+    return EXIT_SUCCESS;
+}
+

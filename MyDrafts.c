a7c2b146130936db23fa7642bc107ba1cb4901d0
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-04-12 15:23:00 +0100

    drafts update

--- MyDrafts.c
+++ MyDrafts.c
@@ -1271,3 +1271,30 @@ int main(int argc, char* argv[]) {
     return EXIT_SUCCESS;
 }
 
+#define NEW_PERMISSION 644 // rw,r,r
+
+void touch(char* fileName) {
+
+    FILE* file = fopen(fileName, "w");
+    if (chmod(fileName, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH) != 0) {
+        printf("unable to change permissions in %s file!\n", fileName);
+    }
+    fclose(file);
+}
+
+int main(int argc, char *argv[]) {
+
+    if (argc < 2) {
+        fprintf(stderr, "usage: %s <FILENAME>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    int i = 1;
+    while (argv[i] != NULL) {
+        touch(argv[i]);
+        i++;
+    }
+
+    return EXIT_SUCCESS;
+}
+

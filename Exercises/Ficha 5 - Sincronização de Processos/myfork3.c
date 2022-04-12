e11578d8001c9bc4e20a1568b8ed39f173c64318
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-04-12 17:31:14 +0100

    exercise 3

--- Exercises/Ficha 5 - Sincronização de Processos/myfork3.c
+++ Exercises/Ficha 5 - Sincronização de Processos/myfork3.c
@@ -2,26 +2,33 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+
 int main(int argc, char* argv[]) {
-  pid_t pid;
-  int value = 0;
-  if ((pid = fork()) == -1) {
-    perror("fork");
-    return EXIT_FAILURE;
-  }
-  else if (pid == 0) {
-    /* child process */
-    value = 1;
-    printf("CHILD: value = %d, addr = %p\n", value, &value);
-    return EXIT_SUCCESS;
-}
-else {
-    /* parent process */
-    if (waitpid(pid, NULL, 0) == -1) {
-      perror("wait");
-      return EXIT_FAILURE;
+
+    pid_t pid;
+    int value = 0;
+    if ((pid = fork()) == -1) {
+        perror("fork");
+        return EXIT_FAILURE;
     }
-    printf("PARENT: value = %d, addr = %p\n", value, &value);
+    else if (pid == 0) {
+        /* child process */
+        value = 1;
+        printf("CHILD: value = %d, addr = %p\n", value, &value);
+    }
+    else {
+        /* parent process */
+        if (waitpid(pid, NULL, 0) == -1) {
+            perror("wait");
+            return EXIT_FAILURE;
+        }
+        printf("PARENT: value = %d, addr = %p\n", value, &value);
+    }
+
+    /*  Apesar de terem o mesmo endereço, a memória modificada pelo filho 
+        Não é a mesma que o pai conhece                                         */
+
+    // CHILD: value = 1, addr = 0x16f21f688
+    // PARENT: value = 0, addr = 0x16f21f688
     return EXIT_SUCCESS;
-  }
 }

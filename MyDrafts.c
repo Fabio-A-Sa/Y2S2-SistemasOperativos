2821e9a8a0420a54e1e7ed202bc512563abe5fb6
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-04-12 19:03:44 +0100

    drafts update

--- MyDrafts.c
+++ MyDrafts.c
@@ -1333,3 +1333,33 @@ int main(int argc, char* argv[]) {
     return EXIT_SUCCESS;
 }
 
+int main(int argc, char* argv[]) {
+
+    pid_t pid;
+    int value = 0;
+    if ((pid = fork()) == -1) {
+        perror("fork");
+        return EXIT_FAILURE;
+    }
+    else if (pid == 0) {
+        /* child process */
+        value = 1;
+        printf("CHILD: value = %d, addr = %p\n", value, &value);
+    }
+    else {
+        /* parent process */
+        if (waitpid(pid, NULL, 0) == -1) {
+            perror("wait");
+            return EXIT_FAILURE;
+        }
+        printf("PARENT: value = %d, addr = %p\n", value, &value);
+    }
+
+    /*  Apesar de terem o mesmo endereço, a memória modificada pelo filho 
+        Não é a mesma que o pai conhece                                         */
+
+    // CHILD: value = 1, addr = 0x16f21f688
+    // PARENT: value = 0, addr = 0x16f21f688
+    return EXIT_SUCCESS;
+}
+
